5fce3290d2c2f5208c0e4344e566e7062a478b8d
ottafregu <104422433+ottafregu@users.noreply.github.com>
2023-05-16 13:04:45 +0000

    16 maggio

--- script.js
+++ script.js
@@ -15,48 +15,6 @@ function svuotaCarrello() {
   carrello.innerHTML = 0;
 }
 
-/*burger menu*/
-var Menu = {
-  el: {
-    menu: $('.menu'),
-    menuTop: $('.menu-top'),
-    menuClose: $('.menu-close'),
-    menuMiddle: $('.menu-middle'),
-    menuBottom: $('.menu-bottom'),
-    menuText: $('.menu-text')
-  },
-  
-  init: function() {
-    Menu.bindUIactions();
-  },
-  
-  bindUIactions: function() {
-    Menu.el.menu
-        .on(
-          'click',
-        function(event) {
-        Menu.activateMenu(event);
-        event.preventDefault();
-      }
-    );
-  },
-  
-  activateMenu: function() {
-    Menu.el.menuTop.toggleClass('menu-top-expand expand');
-    Menu.el.menuMiddle.toggleClass('menu-middle-expand expand');
-    Menu.el.menuBottom.toggleClass('menu-bottom-expand expand'); 
-    Menu.el.menuText.toggleClass('menu-text-expand');
-    Menu.el.menuClose.toggleClass('menu-close-visible');
-  }
-};
-  
-  //Stop menu item click closing the menu
-  $(".menu .menu-global").click(function(e) {
-      e.stopPropagation();
-});
-
-Menu.init();
-
 /*swiper*/
 var swiper = new Swiper(".mySwiper", {
       spaceBetween: 30,
@@ -74,3 +32,16 @@ var swiper = new Swiper(".mySwiper", {
         prevEl: ".swiper-button-prev",
       },
     },)
+
+/*jQuery ready compatto*/
+    $(function
+    ()
+    {
+    $("h1").click(function
+    ()
+    {
+    $("#prodotto1").slideDown();
+    $("#prodotto2").slideDown("slow");
+    $("#prodotto3").slideDown(3000);
+    });
+    });
