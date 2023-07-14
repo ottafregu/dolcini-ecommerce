58122e102f0f35e30232012531bc051aa7c309ca
ottafregu <104422433+ottafregu@users.noreply.github.com>
2023-07-14 07:20:29 +0000

    14 Luglio 23 - aggiornamento funzioni ccarrello

--- script.js
+++ script.js
@@ -93,21 +93,45 @@ var swiper = new Swiper(".mySwiper", {
 
 
                                                // prodotti
-function visualizzaTaglie() {
-      var taglie = document.getElementById("taglie");
-      var taglieup = document.getElementById("taglieup");
-      var tagliedown = document.getElementById("tagliedown");
-      if (taglie.style.display === "none") {
-        taglie.style.display = "block";
-        taglieup.style.display = "block";
-        tagliedown.style.display = "none";
-      } else {
-        taglie.style.display = "none";
-        taglieup.style.display = "none";
-        tagliedown.style.display = "block";
-      }
-    }
-function cambiaTaglia() {
-      var taglia = document.getElementById("taglia");
-      taglia.value = event.target.innerHTML;
-    }
+var quantity1Input = document.getElementById("quantity1");
+var quantity2Input = document.getElementById("quantity2");
+var quantity3Input = document.getElementById("quantity3");
+var totalCost1Output = document.getElementById("totalCost1");
+var totalCost2Output = document.getElementById("totalCost2");
+var totalCost3Output = document.getElementById("totalCost3");
+var subTotalOutput = document.getElementById("subTotal");
+var shippingCostOutput = document.getElementById("shippingCost");
+var grandTotalOutput = document.getElementById("grandTotal");
+
+quantity1Input.addEventListener("input", updateCosts);
+quantity2Input.addEventListener("input", updateCosts);
+quantity3Input.addEventListener("input", updateCosts);
+
+function updateCosts() {
+  var quantity1 = parseInt(quantity1Input.value);
+  var quantity2 = parseInt(quantity2Input.value);
+  var quantity3 = parseInt(quantity3Input.value);
+  
+  var cost1 = 20;
+  var cost2 = 13;
+  var cost3 = 15;
+  
+  var totalCost1 = quantity1 * cost1;
+  var totalCost2 = quantity2 * cost2;
+  var totalCost3 = quantity3 * cost3;
+  
+  totalCost1Output.textContent = "€" + totalCost1;
+  totalCost2Output.textContent = "€" + totalCost2;
+  totalCost3Output.textContent = "€" + totalCost3;
+  
+  var subTotal = totalCost1 + totalCost2 + totalCost3;
+  var shippingCost = 8;
+  var grandTotal = subTotal + shippingCost;
+  
+  subTotalOutput.textContent = "€" + subTotal;
+  shippingCostOutput.textContent = "€" + shippingCost;
+  grandTotalOutput.textContent = "€" + grandTotal;
+}
+
+
+
